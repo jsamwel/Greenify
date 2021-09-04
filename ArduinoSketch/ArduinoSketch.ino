4dcf7bce32801b6c61d05ec5698b93b206896bfa
jsamwel <7791315+jsamwel@users.noreply.github.com>
2021-09-04 17:24:11 +0200

    Version 1
    Version 1 of the software that reads the moisture

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -1,108 +1,114 @@
-#include <Client.h>
-#include <SPI.h>
-#include <WiFiNINA.h>
-#include <CooperativeMultitasking.h>
-#include <MQTTClient.h>
+/*
+  ArduinoMqttClient - WiFi Simple Sender
+  This example connects to a MQTT broker and publishes a message to
+  a topic once a second.
+  The circuit:
+  - Arduino MKR 1000, MKR 1010 or Uno WiFi Rev2 board
+  This example code is in the public domain.
+*/
 
-char ssid[] = "JoWiFi";
-char pass[] = "Nespresjo";
-char host[] = "192.168.2.3";
-char clientid[] = "Greenify";
-char username[] = "";
-char password[] = "";
-char topicname[] = "MoistPlant1nName";
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ESP8266_ESP12)
+  #include <ESP8266WiFi.h>
+#endif
 
-CooperativeMultitasking tasks;
-Continuation beginWiFiIfNeeded;
-Continuation connectMQTTClientIfNeeded;
-Continuation light;
-Continuation dark;
-Guard isDark;
-Continuation on;
-Continuation off;
-Guard isLight;
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
-WiFiClient wificlient;
-MQTTClient mqttclient(&tasks, &wificlient, host, 1883, clientid, username, password);
-MQTTTopic topic(&mqttclient, topicname);
+// To connect with SSL/TLS:
+// 1) Change WiFiClient to WiFiSSLClient.
+// 2) Change port value from 1883 to 8883.
+// 3) Change broker value to a server with a known SSL/TLS root certificate 
+//    flashed in the WiFi module.
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "192.168.2.15";
+int        port     = 1883;
+const char topic[]  = "Greenify/plant1n/moisture";
+
+const long interval = 5000;
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+
+const int numReadings = 10;
+int moistureBuffer[numReadings];
+int readingMoisture = 0;
+float moisture;
 
 void setup() {
-  Serial.begin(9600);
-  //
-  while (!Serial) {
-    delay(1000);
-  }
-  //
-  Serial.println("begin wifi");
-  //
-  WiFi.begin(ssid, pass);
-  tasks.after(10000, beginWiFiIfNeeded); // after 10 seconds call beginWiFiIfNeeded()
-  //
-  Serial.println("connect mqtt client");
-  //
-  mqttclient.connect();
-  tasks.after(15000, connectMQTTClientIfNeeded); // after 15 seconds call connectMQTTClientIfNeeded()
-  //
-  pinMode(LED_BUILTIN, OUTPUT);
-  tasks.now(light); // call light() now
+  // setup connection
+  connectCom();
 }
 
 void loop() {
-  tasks.run();
-}
+  // check if the connection is still up and reconnect otherwise
+  if (!mqttClient.connected()){connectCom();}
+  
+  // call poll() regularly to allow the library to send MQTT keep alives which
+  // avoids being disconnected by the broker
+  mqttClient.poll();
 
-void light() {
-  topic.publish("\"light\"");
-  //
-  tasks.ifForThen(isDark, 5000, dark); // if isDark() for 5 seconds then call dark()
-}
+  // to avoid having delays in loop, we'll use the strategy from BlinkWithoutDelay
+  // see: File -> Examples -> 02.Digital -> BlinkWithoutDelay for more info
+  currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time a message was sent
+    previousMillis = currentMillis;
 
-void dark() {
-  topic.publish("\"dark\"");
-  //
-  tasks.now(on); // call on() now
-}
+    // get measurement and average out the values
+    // read from the sensor:
+    readingMoisture = map(analogRead(A0),450,670,100,0);
+ 
+    // calculate the average:
+    moisture = averageMeasure(readingMoisture, moistureBuffer, numReadings);
 
-bool isDark() {
-  return analogRead(A1) < 300;
-}
-
-void on() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  tasks.after(1000, off); // after 1 second call off()
-}
-
-void off() {
-  digitalWrite(LED_BUILTIN, LOW);
-  auto task1 = tasks.after(1000, on); // after 1 second call on()
-  auto task2 = tasks.ifForThen(isLight, 0, light); // if isLight() then call light()
-  tasks.onlyOneOf(task1, task2); // do either task1 or task2
-}
-
-bool isLight() {
-  return analogRead(A1) >= 400;
-}
-
-void beginWiFiIfNeeded() {
-  switch (WiFi.status()) {
-    case WL_IDLE_STATUS:
-    case WL_CONNECTED: tasks.after(30000, beginWiFiIfNeeded); return; // after 30 seconds call beginWiFiIfNeeded() again
-    case WL_NO_SHIELD: Serial.println("no wifi shield"); return; // do not check again
-    case WL_CONNECT_FAILED: Serial.println("wifi connect failed"); break;
-    case WL_CONNECTION_LOST: Serial.println("wifi connection lost"); break;
-    case WL_DISCONNECTED: Serial.println("wifi disconnected"); break;
+    // send message, the Print interface can be used to set the message contents
+    mqttClient.beginMessage(topic);
+    //mqttClient.print("hello ");
+    mqttClient.print(moisture);
+    mqttClient.endMessage();
   }
-  //
-  WiFi.begin(ssid, pass);
-  tasks.after(10000, beginWiFiIfNeeded); // after 10 seconds call beginWiFiIfNeeded() again
+  else if (previousMillis > currentMillis){
+    previousMillis = currentMillis;
+  }
 }
 
-void connectMQTTClientIfNeeded() {
-  if (!mqttclient.connected()) {
-    Serial.println("mqtt client not connected");
-    //
-    mqttclient.connect();
+void connectCom(){
+  // attempt to connect to WiFi network:
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    delay(5000);
   }
-  //
-  tasks.after(30000, connectMQTTClientIfNeeded); // after 30 seconds call connectMQTTClientIfNeeded() again
+
+  if (!mqttClient.connect(broker, port)) {
+    delay(1000);
+  }
+}
+
+float averageMeasure(int measurement, int *measurementBuffer, int bufferSize){
+  float average = 0;
+  int currentValueBuffer;
+
+  // shift elements in buffer to make room for new measurement
+  // add each element in the buffer to average
+  for(int i=bufferSize-1;i>0;i--){
+    measurementBuffer[i] = measurementBuffer[i-1];
+    average += measurementBuffer[i];
+  }
+
+  // add latest measurement to buffer and average
+  measurementBuffer[0] = measurement;
+  average += measurement;
+
+  // calculate and return average
+  return average /= bufferSize;
 }
